003c7d7a6557ddbd938b92302e97c50eff8a68e6
KantaTatstumi <66326252+KantaTatstumi@users.noreply.github.com>
2023-06-20 12:31:22 +0700

    Delete Topic Encapsulation directory

--- Topic Encapsulation/Missing-And-Leaky-Encaps_After/Missing-And-Leaky-Encaps-main/Missing_encapsulation_After/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-
-        Employee emp = new Employee("Adit", "genderless", 21, 1000);
-        emp.display();
-        
-    }
-}
