39fb6433b3390137d1eaef46f700409d04b8943d
KantaTatstumi <66326252+KantaTatstumi@users.noreply.github.com>
2023-06-20 12:30:40 +0700

    Delete missing,incomplete,imperative Abstraction directory

--- missing,incomplete,imperative Abstraction/Kel_11_After/Qari-code-reengineering-master/src/main/java/QrImageEncoder.java
+++
@@ -1,9 +0,0 @@
-// untuk membaca isi data pada parameter yang di input dan mengecheck bila ada null/error pada data
-import io.nayuki.qrcodegen.QrCode;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
-public interface QrImageEncoder {
-    ByteArrayInputStream convert(QrCode qr, int scale, int border, QrImgFormat ImgFormat) throws NullPointerException, IllegalArgumentException, IOException;
-}
